42bccfcfe65f1e02e6fd8b804f44f29d123b3eed
Grégory <ptitgraig@hotmail.com>
2014-06-13 14:53:08 +0200

    integration des bijoux

---
+++ app/index.php
@@ -0,0 +1,327 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <title>Keursha - Bijoutier pour particulier, Paris 19ème.</title>
+    <meta name="description" content="Bijoux, alliances, bagues de fiancaille.">
+    <meta name="author" content="Keursha, fabriqué avec amour par Grégory Pelletey">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <!-- build:css styles/vendor.css -->
+    <!-- bower:css -->
+
+    <!-- endbower -->
+    <!-- endbuild -->
+    <!-- build:css(.tmp) styles/main.css -->
+    <link rel="stylesheet" href="styles/normalize.css">
+    <link rel="stylesheet" href="styles/fonts.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/modal-components.css">
+    <!-- endbuild -->
+
+    
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <link rel="stylesheet" href="css/ie.css">
+    <![endif]-->
+    <script src="scripts/responsive-nav.js"></script>
+  </head>
+  <body>
+    <!--[if lt IE 10]>
+        <p class="browsehappy">Vous utilisez un navigateur quelque peu <strong>vintage</strong>. Pour profiter au mieux du web, <a href="http://browsehappy.com/">mettez à jour votre navigateur</a>.</p>
+    <![endif]-->
+
+
+
+    <header>
+      <a href="#home" class="logo" data-scroll>Keursha</a>
+      <nav class="nav-collapse">
+        <ul>
+          <li class="active"><a href="#accueil" data-scroll>Accueil</a></li>
+          <li><a href="#bijoux" data-scroll>Bijoux</a></li>
+          <li><a href="#temoignage" data-scroll>Témoignages</a></li>
+          <li><a href="#a-propos" data-scroll>A propos</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <section id="accueil">
+      <h1>Joaillerie Keursha, Paris</h1>
+      <p class="small-n-center">
+Keursha n'est pas un site de vente de bijoux , c'est un concept à part pour vous offrir une nouvelle expérience d'achat.  Nous voulons nous distinguer par notre approche de qualité qui vous permettra de prendre le temps de connaître les matériaux, de choisir les pierres mais aussi de vous poser toutes les questions qui vous mèneront à votre bijou.<br />
+        <br/>
+      </p>
+      <div class="social">
+        <a href="http://www.facebook.com" class="fb" title="Retrouvez-moi sur facebook"></a>
+        <a href="http://www.facebook.com" class="twitter" title="Retrouvez-moi sur twitter"></a>
+      </div>
+    </section>
+
+    <section id="bijoux">
+      <h1>Bijoux</h1>
+        <div class="filter-wrapper">
+            <div class="filter-container">
+                <div class="category-landing">
+                    <h2>Votre Sélection</h2>
+                </div>
+                <div class="block-layered-nav">
+                    <div class="block-title">
+                        <a class="toggle-button" href="#filter-menu">Filtre<span class="badge"></span></a>
+                    </div>
+                </div>
+            </div><!-- /container -->
+        </div>
+        <div class="selection">
+            <ul class="nav nav-pills">
+                <li class="active">
+                    <a href="#" class="button" data-filter="*">Tout</a>
+                </li>
+                <li class="dropdown">
+                    <a id="typebijou" role="button" data-toggle="dropdown" href="#" class="dropdown-toggle">
+                        <span>Type de bijou</span>
+                        <b class="caret"></b>
+                    </a>
+                    <ul id="typebijou-menu" class="dropdown-menu" role="menu" aria-labbeledby="typebijou" data-filter-group="typebijou">
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter="" class="button">Tous les types</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".alliance" class="button">Alliances</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".fiancaille" class="button">Bagues de fiancailles</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".bague" class="button">Bagues</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".boucle" class="button">Boucles d'oreilles</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".pendentif" class="button">Pendentifs</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".collier" class="button">Colliers</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a id="materiau" role="button" data-toggle="dropdown" href="#" class="dropdown-toggle">
+                        <span>Matériau</span>
+                        <b class="caret"></b>
+                    </a>
+                    <ul id="materiau-menu" class="dropdown-menu" role="menu" aria-labbeledby="materiau" data-filter-group="materiau">
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter="" class="button">Tout type de matériau</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".platine" class="button">Platine</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".orjaune" class="button">Or jaune</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".orblanc" class="button">Or blanc</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".orrose" class="button">Or rose</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".argent" class="button">Argent</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a id="materiau" role="button" data-toggle="dropdown" href="#" class="dropdown-toggle">
+                        <span>Diamants</span>
+                        <b class="caret"></b>
+                    </a>
+                    <ul id="diamant-menu" class="dropdown-menu" role="menu" aria-labbeledby="diamant" data-filter-group="diamant">
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter="" class="button">Avec ou sans diamant</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".zero-diamant" class="button">Aucun diamant</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".un-diamant" class="button">Un seul diamant</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".plus-un-diamant" class="button">Au moins 2 diamants</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a id="sexe" role="button" data-toggle="dropdown" href="#" class="dropdown-toggle">
+                        <span>Public</span>
+                        <b class="caret"></b>
+                    </a>
+                    <ul id="public-menu" class="dropdown-menu" role="menu" aria-labbeledby="public" data-filter-group="public">
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".mixte" class="button">Mixte</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".femme" class="button">Une femme</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".homme" class="button">Un homme</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a id="prix" role="button" data-toggle="dropdown" href="#" class="dropdown-toggle">
+                        <span>Prix</span>
+                        <b class="caret"></b>
+                    </a>
+                    <ul id="prix-menu" class="dropdown-menu" role="menu" aria-labbeledby="prix" data-filter-group="prix">
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter="" class="button">sans budget</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".inf500" class="button">entre 501 et 1000 &euro;</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".sup500" class="button">entre 1001 et 2000 &euro;</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".sup500" class="button">entre 2001 et 3000 &euro;</a>
+                        </li>
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#" data-filter=".sup500" class="button">&gt; 3000 &euro;</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div id="produits">
+            <?
+            // presentation des produits
+            $lines = file('data/bijoux.json');
+            
+            $data = [];
+            foreach ($lines as $line_num => $line) {
+                $data[$line_num] = json_decode($line, true);
+            }
+            $max = sizeof($data);
+            for ($i = 0; $i < $max; $i++) {
+                echo '
+                    <div class="square '.$data[$i]['type'].' '.$data[$i]['materiau'].' '.$data[$i]['public'].' md-trigger" data-modal="modal-'.$i.'">
+                        <img src="images/bijoux/'.$data[$i]['imgname'].'" width="200" height="200" />
+                        <span class="title">'.$data[$i]['title'].'</span>
+                    </div>';
+            }
+            ?>
+        </div>
+
+        <?
+        // popin
+        for ($i = 0; $i < $max; $i++) {
+            echo '
+                <div class="md-modal md-effect-1" id="modal-'.$i.'">
+                    <div class="md-content">
+                        <h3>'.$data[$i]['title'].'</h3>
+                        <div>
+                            <img src="images/bijoux/'.$data[$i]['imgname'].'" />
+                            <div class="fiche">
+                                <dl>
+                                    <dt>Matériau</dt>
+                                    <dd>'.$data[$i]['materiau'].'</dd>
+                                    <dt>Diamants</dt>
+                                    <dd>'.$data[$i]['diamant'].'</dd>
+                                    <dt>Public</dt>
+                                    <dd>'.$data[$i]['public'].'</dd>
+                                    <dt>Description</dt>
+                                    <dd>'.$data[$i]['description'].'</dd>
+                                    <dt>Prix</dt>
+                                    <dd>'.$data[$i]['price'].'</dd>
+                                </dl>
+                                <button class="btn btn-modal md-close">Contact</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+        }
+        ?>
+        <div class="md-overlay"></div>
+        
+
+    </section>
+
+    <section id="temoignage">
+      <h1>Témoignages</h1>
+    </section>
+
+    <section id="a-propos">
+      <h1>A propos</h1>
+
+      <div class="tabs" data-tabs="">
+        <ul>
+            <li class="tab-active"><a href="#">&Eacute;thique</a></li>
+            <li><a href="#">Savoir-faire</a></li>
+            <li><a href="#">Le fonctionnement</a></li>
+            <li><a href="#">Les valeurs</a></li>
+        </ul>
+        <section class="tab-pane-active">
+            Ethique et tac
+        </section>
+        <section>
+            Savoir faire à repasser
+        </section>
+        <section>
+            Fonctionnement heure menthe Eure
+        </section>
+        <section>
+            Les valeurs de t'acheter une montre
+        </section>
+    </div>
+
+      <p class="small-n-center">
+Notre volonté est de n'offrir que des bijoux de qualité fait uniquement en France. Nous désirons également partager avec vous notre expertise et notre amour du bijou afin de vous offrir ce qui se fait de mieux en joaillerie. <br/>
+La devise de Keursha et de Vous offrir des bijoux de qualité à des prix éthique, valorisant le travail de chaque ouvrier sans profité du consommateur. <br/>
+Nous vous souhaitons une agréable visite en notre compagnie et restons à votre écoute.
+      </p>
+    </section>
+
+
+        
+
+
+
+    <!-- build:js scripts/vendor.js -->
+        <!-- bower:js -->
+        <script src="bower_components/jquery/jquery.js"></script>
+        <!-- endbower -->
+        <!-- endbuild -->
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+
+    <!-- build:js({app,.tmp}) scripts/main.js -->
+    <!--<script type="text/javascript" src="scripts/jquery.stellar.min.js"></script>
+    <script type="text/javascript" src="scripts/waypoints.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>-->
+    <!--<script type="text/javascript" src='scripts/swipe.js'></script>-->
+    <script type="text/javascript" src="scripts/isotope.js"></script>
+    <script type="text/javascript" src='scripts/mansonry-horizontal.js'></script>
+    <script type="text/javascript" src="scripts/dropdown.js"></script>
+    <script type="text/javascript" src="scripts/main.js"></script>
+    <script src="scripts/fastclick.js"></script>
+    <script src="scripts/scroll.js"></script>
+    <script src="scripts/tabs.js"></script>
+    <script src="scripts/fixed-responsive-nav.js"></script>
+    <script src="scripts/modal-effects.js"></script>
+    <!-- endbuild -->
+  </body>
+</html>
